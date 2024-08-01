74a868ff2d9df3e91f3f7314a498715fad7ae04e
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2024-08-01 19:32:05 +0530

    update samesite preference to none

--- backend/utils/sendToken.ts
+++ backend/utils/sendToken.ts
@@ -17,7 +17,7 @@ export const sendToken = (user: IUser, res: Response, message: string, statusCod
     res.status(statusCode).cookie("auth-token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        sameSite: 'none',
         expires: new Date(Date.now() + cookie_expire * 24 * 60 * 60 * 1000),
     }).json({
         success: true,
