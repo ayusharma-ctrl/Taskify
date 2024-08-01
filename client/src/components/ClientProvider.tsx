e97b0af707204a62fd2e331634e74fd78ab79f05
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2024-08-01 19:06:58 +0530

    set global default headers

--- client/src/components/ClientProvider.tsx
+++ client/src/components/ClientProvider.tsx
@@ -4,6 +4,10 @@ import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../store';
+import axios from 'axios';
+
+// set global default headers for axios
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 const ClientProvider = ({ children }: { children: ReactNode }) => {
     return (
