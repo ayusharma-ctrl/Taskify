ff2953c7653c94a39f5d6d9bf668ee78a20d1772
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2024-08-01 19:22:30 +0530

    update samesite preference to set cookie

--- backend/dist/utils/sendToken.js
+++ backend/dist/utils/sendToken.js
@@ -17,6 +17,7 @@ const sendToken = (user, res, message, statusCode = 200) => {
     res.status(statusCode).cookie("auth-token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         expires: new Date(Date.now() + cookie_expire * 24 * 60 * 60 * 1000),
     }).json({
         success: true,
