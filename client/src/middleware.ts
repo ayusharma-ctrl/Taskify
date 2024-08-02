b9241d8eead63181043a571233bfef4536960984
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2024-08-02 16:57:31 +0530

    added logs

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -4,12 +4,14 @@ import type { NextRequest } from "next/server";
 
 async function checkUserAuthentication(request: NextRequest): Promise<boolean> {
     const cookie = request.cookies.get("auth-token");
+    console.log("Cookie-name", cookie?.name);
     if (!cookie) return false;
     return true;
 }
 
 export async function middleware(request: NextRequest) {
     const userAuthenticated = await checkUserAuthentication(request);
+    console.log("userAuthenticated", userAuthenticated);
     const currentPath = request.nextUrl.pathname;
 
     if (!userAuthenticated && currentPath.startsWith("/dashboard")) {
